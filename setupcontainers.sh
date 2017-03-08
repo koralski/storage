759ec9705fb6c0f8b29e73be5618f548a328cf2c
Atanas Koralski <koralski@gmail.com>
2017-03-07 16:03:27 -0800

    update script

--- setupcontainers.sh
+++ setupcontainers.sh
@@ -1,4 +1,11 @@
 echo Start_script
+
+while !command -v docker >/dev/null 2>&1
+do
+  echo wait for docker
+  sleep 5
+done
+
 docker --version
 docker load -i nginx.tar
 docker run -d -p 80:80 nginx
