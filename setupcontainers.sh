3b6f26776a4fdde500ceed8c15183277553b6a35
Atanas Koralski <koralski@gmail.com>
2017-03-15 15:35:55 -0700

    update

--- setupcontainers.sh
+++ setupcontainers.sh
@@ -1,6 +1,12 @@
-echo Start_script
-imageName=$1
+#!/bin/bash
+# Parameters: 
+#   dockerOptions - the docker run options, including port mapping and image name (ex. '-p 80:80 nginx:latest')
+#   imageFile - the docker image file name what is downloaded with the script (ex. nginx.tar)
+dockerOptions=$1
 imageFile=$2
+
+echo Start_script
+# Check is imageFile is downloaded successfully
 if [ -f `pwd`/$imageFile ]
 then
   echo image download sucessfully to `pwd`/$imageFile
@@ -8,21 +14,22 @@ else
   echo image download failed
 fi
 
+# wait for docker extension to be installed
 while ! [ -f /usr/bin/docker ]
 do
   sleep 5
 done
-
 echo docker installed
 
-while ! docker ps  >/dev/null 2>&1
+# wait for docker daemon to start
+while ! docker ps >/dev/null 2>&1
 do
   sleep 5
 done
-
-echo docker is running
+echo docker daemon is running
 docker --version
 
+# load the local docker image
 if docker load -i `pwd`/$imageFile
 then
   echo image is loaded sucessfully
@@ -31,7 +38,7 @@ else
   echo "current folder is: `pwd`"
 fi
 
-if docker run -d -p 80:80 $imageName
+if docker run -d $dockerOptions
 then
   echo running sucessfully
 else
